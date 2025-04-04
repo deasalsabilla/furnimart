94a080364fb72811ce5082077101703f58a71947
Dea Salsabilla <deasalsabilla19@gmail.com>
2025-04-04 13:13:09 +0700

    fitur continue shopping cart

--- cart.php
+++ cart.php
@@ -248,7 +248,7 @@
             </tbody>
           </table>
           <div class="checkout_btn_inner float-right">
-            <a class="btn_1" href="#">Continue Shopping</a>
+            <a class="btn_1" href="belanja.php">Continue Shopping</a>
             <a class="btn_1 checkout_btn_1" href="#">Proceed to checkout</a>
           </div>
         </div>
