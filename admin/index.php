14a0c88d32151a2ebb9c2707ac9cb14e32a3dbca
unknown <acceresik@gmail.com>
2025-04-04 15:51:30 +0700

    Menyesuaikan Tampilan Beranda

--- admin/index.php
+++ admin/index.php
@@ -265,7 +265,7 @@ if (!isset($_SESSION["login"])) {
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">SecretAdmin</a>
+      Designed by <a href="#">SecretAdmin</a>
     </div>
   </footer><!-- End Footer -->
 
