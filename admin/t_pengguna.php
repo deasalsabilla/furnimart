0bf582b34592153b06163c10d89d6d7ddf151e14
Dea Salsabilla <deasalsabilla19@gmail.com>
2025-04-05 17:41:26 +0700

    mengganti gambar di user dropdown

--- admin/t_pengguna.php
+++ admin/t_pengguna.php
@@ -254,7 +254,7 @@ if (isset($_POST['simpan'])) {
                 <li class="nav-item dropdown pe-3">
 
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                        <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+                        <img src="assets/img/user.jpg" alt="Profile" class="rounded-circle">
                         <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
                     </a><!-- End Profile Iamge Icon -->
 
